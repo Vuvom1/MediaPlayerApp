65539fca3117ae48a15abeb286a510eb0f151fa1
Vuvom1 <146900726+Vuvom1@users.noreply.github.com>
2023-12-13 21:32:28 +0700

    Update MusicPlayer.sql

--- MusicPlayer.sql
+++ MusicPlayer.sql
@@ -1,97 +1,24 @@
--- phpMyAdmin SQL Dump
--- version 5.2.1
--- https://www.phpmyadmin.net/
---
--- Máy chủ: localhost
--- Thời gian đã tạo: Th12 13, 2023 lúc 03:30 PM
--- Phiên bản máy phục vụ: 10.4.28-MariaDB
--- Phiên bản PHP: 8.2.4
+CREATE TABLE MUSIC(
+    ID INT PRIMARY KEY,
+    NAME VARCHAR(50),
+    FILEPATH VARCHAR(255)
+);
 
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-START TRANSACTION;
-SET time_zone = "+00:00";
+CREATE TABLE PLAYLIST (
+    ID INT PRIMARY KEY,
+    NAME VARCHAR(50)
+);
 
+CREATE TABLE MUSIC_PLAYLIST(
+    ID INT PRIMARY KEY,
+    MUSIC_ID INT,
+    PLAYLIST_ID INT
+);
 
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
+ALTER TABLE MUSIC_PLAYLIST
+ADD CONSTRAINT FK_MP_MS
+FOREIGN KEY (MUSIC_ID) REFERENCES MUSIC  (ID);
 
---
--- Cơ sở dữ liệu: `MusicPlayer`
---
-
--- --------------------------------------------------------
-
---
--- Cấu trúc bảng cho bảng `MUSIC`
---
-
-CREATE TABLE `MUSIC` (
-  `ID` int(11) NOT NULL,
-  `NAME` varchar(50) DEFAULT NULL,
-  `FILEPATH` varchar(255) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
--- --------------------------------------------------------
-
---
--- Cấu trúc bảng cho bảng `MUSIC_PLAYLIST`
---
-
-CREATE TABLE `MUSIC_PLAYLIST` (
-  `ID` int(11) NOT NULL,
-  `MUSIC_ID` int(11) DEFAULT NULL,
-  `PLAYLIST_ID` int(11) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
--- --------------------------------------------------------
-
---
--- Cấu trúc bảng cho bảng `PLAYLIST`
---
-
-CREATE TABLE `PLAYLIST` (
-  `ID` int(11) NOT NULL,
-  `NAME` varchar(50) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
---
--- Chỉ mục cho các bảng đã đổ
---
-
---
--- Chỉ mục cho bảng `MUSIC`
---
-ALTER TABLE `MUSIC`
-  ADD PRIMARY KEY (`ID`);
-
---
--- Chỉ mục cho bảng `MUSIC_PLAYLIST`
---
-ALTER TABLE `MUSIC_PLAYLIST`
-  ADD PRIMARY KEY (`ID`),
-  ADD KEY `FK_MP_MS` (`MUSIC_ID`),
-  ADD KEY `FK_MP_PL` (`PLAYLIST_ID`);
-
---
--- Chỉ mục cho bảng `PLAYLIST`
---
-ALTER TABLE `PLAYLIST`
-  ADD PRIMARY KEY (`ID`);
-
---
--- Các ràng buộc cho các bảng đã đổ
---
-
---
--- Các ràng buộc cho bảng `MUSIC_PLAYLIST`
---
-ALTER TABLE `MUSIC_PLAYLIST`
-  ADD CONSTRAINT `FK_MP_MS` FOREIGN KEY (`MUSIC_ID`) REFERENCES `MUSIC` (`ID`),
-  ADD CONSTRAINT `FK_MP_PL` FOREIGN KEY (`PLAYLIST_ID`) REFERENCES `PLAYLIST` (`ID`);
-COMMIT;
-
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+ALTER TABLE MUSIC_PLAYLIST
+ADD CONSTRAINT FK_MP_PL
+FOREIGN KEY (PLAYLIST_ID) REFERENCES PLAYLIST  (ID);
